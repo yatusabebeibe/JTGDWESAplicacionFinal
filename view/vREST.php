3c13aa3dff5056f65529cdbcaf6f9f9ba235aa90
yatusabebeibe <jesus.temgal@educa.jcyl.es>
2026-01-18 03:32:34 +0100

    chore: añadir comentario

--- view/vREST.php
+++ view/vREST.php
@@ -1,6 +1,7 @@
 <div class="gridLayout">
     <section>
         <?php $fotoNasa = $avREST["nasa"] ?>
+        <!-- Favoritas 2019-02-12, 2019-02-13, 2018-05-24 -->
         <form method="post" id="nasa" name="nasa">
             <input type="date" name="fecha" id="fecha" value="<?= $fotoNasa->getFecha() ?>" max="<?= date('Y-m-d') ?>" class="obligatorio">
             <input type="submit" value="Enviar">
