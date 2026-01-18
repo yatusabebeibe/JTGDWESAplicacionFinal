bc18cb5cfcc2bddb0280e8fc44dae301b89f05f1
yatusabebeibe <jesus.temgal@educa.jcyl.es>
2026-01-18 03:31:53 +0100

    fix: solucionar error en caso de que todavia no se haya subido imagen de hoy

--- model/REST.php
+++ model/REST.php
@@ -8,9 +8,15 @@ class REST {
         $curl = curl_init("https://api.nasa.gov/planetary/apod?date=$fecha&api_key=".self::NASA_KEY);
 
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
         $respuesta = curl_exec($curl);
         $datos = json_decode($respuesta, true);
+
+        if (isset($datos["code"]) && ($datos["code"] == "404" || $datos["code"] == "400")) {
+            // Si la fecha actual no tiene foto, devolvemos la del día anterior (si acaba de cambiar de día puede que aún no esté disponible)
+            return self::getFotoDiaNasa(date('Y-m-d', strtotime($fecha . ' -1 day'))); // Llamada recursiva
+        }
 
         return new ImagenNasa(
             $datos['date'],
