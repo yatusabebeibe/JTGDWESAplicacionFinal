d770f0d90adcafdb108e6386e27d88bb7002ba7d
yatusabebeibe <jesus.temgal@educa.jcyl.es>
2026-01-18 05:28:56 +0100

    refactor: mejorar la interfaz de inicio publico

--- view/vInicioPublico.php
+++ view/vInicioPublico.php
@@ -1,7 +1,16 @@
-<div class="hero-text">
+<div class="hero-text" style="width: 100%;">
     <h1>Bienvenido a Inicio Publico</h1>
-    <div class="imagenesInicio">
-        <img src="./webroot/images/modelo.png" alt="">
-        <img src="./webroot/images/diagramaDeClases.png" alt="">
+    <link rel="stylesheet" href="./webroot/css/imageModal.css">
+    <div class="carouselImagenes">
+        <input type="radio" name="rd" id="rd1">
+        <input type="radio" name="rd" id="rd2">
+        <input type="radio" name="rd" id="rd3">
+
+        <div class="imagenes">
+            <img src="./webroot/images/modelo.png" alt="">
+            <img src="./webroot/images/diagramaDeClases.png" alt="">
+            <img src="./webroot/images/diagramaDeClasesPropio.png" alt="">
+        </div>
     </div>
+    <script src="./webroot/js/imageModal.js"></script>
 </div>
