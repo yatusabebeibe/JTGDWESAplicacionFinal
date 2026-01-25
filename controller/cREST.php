7e460ae42807d401b6009e2f7e0dd4de835f65ba
yatusabebeibe <jesus.temgal@educa.jcyl.es>
2026-01-25 17:01:35 +0100

    fix: validar que haya un usuario logueado

--- controller/cREST.php
+++ controller/cREST.php
@@ -5,6 +5,16 @@
  * @since 16/01/2026
  */
 
+// Si no hay un usuario logueado, redirigimos al login
+if (! isset($_SESSION["usuarioDAWJTGDAplicacionFinal"])) {
+    $_SESSION["paginaAnterior"] = $_SESSION["paginaEnCurso"];
+    $_SESSION["paginaEnCurso"] = "login";
+
+    // Redirigimos
+    header("Location: index.php");
+    exit;
+}
+
 // Si se ha pulsado el botón de volver, redirigimos a la página anterior
 if(isset($_REQUEST['volver'])){
     $temp = $_SESSION['paginaEnCurso'];
