419a059b4955d61bb72fb0ea7d131ba6f210a1f7
yatusabebeibe <jesus.temgal@educa.jcyl.es>
2026-02-04 12:59:03 +0100

    feat: añadir modelo fisico de datos en inicio publico

--- view/vInicioPublico.php
+++ view/vInicioPublico.php
@@ -6,12 +6,14 @@
         <input type="radio" name="rd" id="rd2">
         <input type="radio" name="rd" id="rd3">
         <input type="radio" name="rd" id="rd4">
+        <input type="radio" name="rd" id="rd5">
 
         <div class="imagenes">
             <img src="./webroot/images/diagramaDeCasosDeUso.png" alt="">
             <img src="./webroot/images/arbolDeNavegacion.png" alt="">
             <img src="./webroot/images/diagramaDeClasesPropio.png" alt="">
             <img src="./webroot/images/usoSession.png" alt="">
+            <img src="./webroot/images/modeloFisicoDeDatos.png" alt="">
         </div>
     </div>
     <script src="./webroot/js/imageModal.js"></script>
